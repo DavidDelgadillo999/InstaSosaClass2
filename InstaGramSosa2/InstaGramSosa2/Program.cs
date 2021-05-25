6b1b9fa834f2bce72c88dc61be69c5099997428f
David <inkudave@gmail.com>
2021-05-25 10:17:58 -0600

    This is my first assignment for Sosa class 2

---
+++ InstaGramSosa2/InstaGramSosa2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InstaGramSosa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOf3Numbers(1, 2, 3));
+        }
+
+        public static int SumOf3Numbers(int a, int b, int c)
+        {
+            int result = a + b + c;
+
+            return result;
+        }
+    }
+}
