00777731864c7e23539c92f326d3aa9f1b49af5d
David <inkudave@gmail.com>
2021-05-25 11:30:09 -0600

    This is my 5th assignment class 2

--- InstaGramSosa2/InstaGramSosa2/Models/Users.cs
+++ InstaGramSosa2/InstaGramSosa2/Models/Users.cs
@@ -2,7 +2,7 @@
 namespace InstaGramSosa2.Models
 {
     
-    public class Users
+    public class Usersw
         {
             public int UserId { get; set; }
             public string UserName { get; set; }
