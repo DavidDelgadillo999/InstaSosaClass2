e8061d2e29211e0940eda627ba4226c5f15978a5
David <inkudave@gmail.com>
2021-05-25 10:42:42 -0600

    This is my second assignment for class 2

--- InstaGramSosa2/InstaGramSosa2/Program.cs
+++ InstaGramSosa2/InstaGramSosa2/Program.cs
@@ -6,14 +6,43 @@ namespace InstaGramSosa2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SumOf3Numbers(1, 2, 3));
-        }
+            Users myUser = new Users();
+            myUser.UserId = 1;
+            myUser.UserName = "Mario Sosa";
 
-        public static int SumOf3Numbers(int a, int b, int c)
-        {
-            int result = a + b + c;
-
-            return result;
+            Console.WriteLine(myUser.UserId);
+            Console.WriteLine(myUser.UserName);
         }
     }
+}
+
+public class Users
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+}
+public class Roles 
+{
+    public string RokeId { get; set; }
+    public string RoleName { get; set; }
+}
+public class Images
+{
+    public string ImageId { get; set; }
+    public string ImageName { get; set; }
+}
+public class Rules
+{
+    public string RulesId { get; set; }
+    public string RuleName { get; set; }
+}
+public class Posts
+{
+    public string PostId { get; set; }
+    public string PostDate { get; set; }
+}
+public class Comments
+{
+    public string CommentId { get; set; }
+    public string TheComment { get; set; }
 }
